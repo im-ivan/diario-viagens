816609a814d40da6831a9e4c485d7c09c7a0dc4e
Ivan C <75918347+im-ivan@users.noreply.github.com>
2022-06-06 17:02:48 -0300

    Revert "Criação de página de termos de uso"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { TermosDeUsoComponent } from './core/components/termos-de-uso/termos-de-uso.component';
 
-const routes: Routes = [
-{path: 'termos-de-uso', component: TermosDeUsoComponent},
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
