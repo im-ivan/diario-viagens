0c71174fb89eb3444e693d1e85510c3a8e2c44fd
IaraBeco <iarabeco82@gmail.com>
2022-06-11 21:37:58 -0300

    inclusão da landing page

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoaderComponent } from './components/loader/loader.component';
 import { MaterialModule } from './material.module';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     
     LoaderComponent,
+    HomeComponent,
   ],
   imports: [CommonModule, MaterialModule],
   exports: [LoaderComponent],
