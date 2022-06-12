9854908cafa005e871737ae7bf39c2e022d90a2a
IaraBeco <iarabeco82@gmail.com>
2022-06-11 21:37:58 -0300

    inclusão da landing page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TermosDeUsoComponent } from './core/components/termos-de-uso/termos-de-uso.component';
+import { HomeComponent} from '../app/shared/components/home/home.component'
 
 const routes: Routes = [
 {path: 'termos-de-uso', component: TermosDeUsoComponent},
+{path: 'home', component: HomeComponent},
 ];
 
 @NgModule({
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoaderComponent } from './components/loader/loader.component';
 import { MaterialModule } from './material.module';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     // recursos que fazem parte do módulo (componentes, pipes, diretivas)
     LoaderComponent,
+    HomeComponent,
   ],
   imports: [CommonModule, MaterialModule],
   exports: [LoaderComponent],
